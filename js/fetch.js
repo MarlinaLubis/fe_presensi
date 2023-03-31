e2b046ec687ab3ffa54f27e215a968bbc5742a4b
Marlina Lubis <lubismarlina5@gmail.com>
2023-03-31 16:40:42 +0700

    Latihan-Marlina

--- js/fetch.js
+++ js/fetch.js
@@ -1,5 +1,5 @@
 import { get } from "https://bukulapak.github.io/api/process.js"; 
-let urlAPI = "https://marlinapp.herokuapp.com/presensi";
+let urlAPI =  "https://marlinapp.herokuapp.com/presensi1";
 get(urlAPI,isiTablePresensi);
 function isiTablePresensi(results){
     console.log(results);
