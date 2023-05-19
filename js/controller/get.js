7b0ae3fec4bc9528f8fb9f91dcc3d130f898d54e
Marlina Lubis <lubismarlina5@gmail.com>
2023-05-19 21:06:08 +0700

    Marlina

--- js/controller/get.js
+++ js/controller/get.js
@@ -9,14 +9,14 @@ function isiRow(value) {
     // console.log(value.jam_kerja)
     let content = isiTabel
                 .replace("#NAMA#", value.biodata.nama)
-                .replace("#NOHP#", value.biodata.phone_number ? value.biodata.phone_number : value.phone_number ? value.phone_number : "#NOHP#")
+                .replace("#NOHP#", value.biodata.phone_number)
                 .replace("#JABATAN#", value.biodata.jabatan)
                 .replace("#LOKASI#", value.location)
                 .replace("#STATUS#", value.checkin)
                 .replace("#HARIKERJA#", value.biodata.hari_kerja?value.biodata.hari_kerja:"#HARIKERJA#")
                 .replace("#JAMKERJA#", value.biodata.hari_kerja ? value.biodata.jam_kerja[0].durasi : "#JAMKERJA#")
-                .replace("#JAMMASUK#", value.biodata.hari_kerja ? value.biodata.jam_kerja[0].jam_masuk : "#JAMMASUK#")
-                .replace("#JAMKELUAR#", value.biodata.hari_kerja ? value.biodata.jam_kerja[0].jam_keluar : "#JAMKELUAR#")
+                .replace("#JAMMASUK#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].jam_masuk : "#JAMMASUK#")
+                .replace("#JAMKELUAR#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].jam_keluar : "#JAMKELUAR#")
                 .replace("#WARNA#", getRandomColor())
                 .replace(/#WARNALOGO#/g, getRandomColorName());
         addInner("iniTabel", content);
